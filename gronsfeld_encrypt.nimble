01e7738addf0205e972990016c7d0ce8212dc789
nicolangelo damico <damiconicola98@gmail.com>
2023-07-21 15:02:08 -0400

    first commit

---
+++ gronsfeld_encrypt.nimble
@@ -0,0 +1,14 @@
+# Package
+
+version       = "0.1.0"
+author        = "nicolangelo damico"
+description   = "A new awesome nimble package"
+license       = "MIT"
+srcDir        = "src"
+bin           = @["gronsfeld_encrypt"]
+
+
+# Dependencies
+
+requires "nim >= 1.6.14"
+requires "cliche >= 1.0.0"
---
+++ src/gronsfeld_encrypt.nim
@@ -0,0 +1,39 @@
+import cliche, os, strutils,tables, sequtils,algorithm, sugar, strformat
+
+proc gronsfeldEncrypt(data: string, key: string): string=
+  result = ""
+  if data.len != key.len: raise newException(IOError,"la clave y el text deven ser del mismo tamaño")
+  if data.isEmptyOrWhitespace or key.isEmptyOrWhitespace: raise newException(IOError,"deve proporcionar todos los datos requeridos")
+  
+  let aToj = {'a'..'j'}
+  let qToz = {'q'..'z'}
+  let aToz = {'a'..'z'}.toSeq
+
+  var encryptKeys = {1: {'0'..'9','k'..'z'}.toSeq,2: aToz.reversed, 3: {'a'..'p'}.toSeq}.toTable
+  encryptKeys[3].insert({'0'..'9'}.toSeq,encryptKeys[3].len)
+
+  let auxKeys = collect:
+    for i,v in aToj.toSeq.pairs: {v: $i}
+  
+  let auxKeys2 = collect:
+    for i,v in qToz.toSeq.pairs: {v: $i}
+
+  for i,v in data.pairs:
+    let posKey = ($key[i]).parseInt
+    let currentKeys = encryptKeys[posKey]
+    var j: int
+    if posKey == 1 and v.toLowerAscii in aToj: j = currentKeys.find(auxKeys[v][0])
+    elif posKey == 3 and v.toLowerAscii in qToz: j = currentKeys.find(auxKeys2[v][0])
+    else: 
+      j = currentKeys.find(v.toLowerAscii)
+      result &= aToz[j]
+      continue  
+
+    result &= currentKeys[j]
+
+
+when isMainModule:
+  let params = commandLineParams()
+  params.getOpt (k: " ")
+  let data = paramStr(2)
+  echo fmt"resultado: {gronsfeldEncrypt(data,k)}, data original: {data}, clave: {k}"
